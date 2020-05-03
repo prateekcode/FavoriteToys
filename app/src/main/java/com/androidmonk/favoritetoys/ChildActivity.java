d403f6316ab0fe9df154ce776a6ba90f4ebf5e4c
Prateek <pratiekray@gmail.com>
2020-05-03 15:13:36 +0530

    Commit 1

---
+++ app/src/main/java/com/androidmonk/favoritetoys/ChildActivity.java
@@ -0,0 +1,85 @@
+package com.androidmonk.favoritetoys;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ChildActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        mTextView = findViewById(R.id.tv_display);
+
+        Intent intentThatStarted = getIntent();
+        if (intentThatStarted.hasExtra(Intent.EXTRA_TEXT)){
+            String textEntered = intentThatStarted.getStringExtra(Intent.EXTRA_TEXT);
+            mTextView.setText(textEntered);
+        }
+
+    }
+
+    public void onClickOpenWebAddress(View view) {
+        Toast.makeText(this, "Open Web Address", Toast.LENGTH_SHORT).show();
+        String urlAsString = "http://www.udacity.com";
+        openWebPage(urlAsString);
+    }
+
+    public void onClickOpenAddress(View view){
+        String addressString = "1600 Amphitheatre Parkway, CA";
+
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme("geo")
+                .path("0,0")
+                .query(addressString);
+        Uri addressUri = builder.build();
+        showMap(addressUri);
+
+        Toast.makeText(this, "Open Location in Map", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickOpenShare(View view){
+        String shareText = "Hello there";
+        shareText(shareText);
+        Toast.makeText(this, "Share text to App", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onCreateOwn(View view){
+        Toast.makeText(this, "Create your own", Toast.LENGTH_SHORT).show();
+    }
+
+    public void openWebPage(String url){
+        Uri uri = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+    }
+
+    public void showMap(Uri uri){
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if (intent.resolveActivity(getPackageManager())!=null){
+            startActivity(intent);
+        }
+    }
+
+    public void shareText(String text){
+        String mimeType = "text/plain";
+        String title = "Learn how to share";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setChooserTitle(title)
+                .setType(mimeType);
+    }
+
+}
--- app/src/main/java/com/androidmonk/favoritetoys/MainActivity.java
+++ app/src/main/java/com/androidmonk/favoritetoys/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mToysListTextView;
-    private Button mOpenGithubProject, mOpenRecyclerView;
+    private EditText mEntryText;
+    private Button mOpenGithubProject, mOpenRecyclerView, mDoExplicit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +22,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mToysListTextView = (TextView) findViewById(R.id.tv_toy_name);
+        mEntryText = findViewById(R.id.et_entry_text);
+        mDoExplicit = findViewById(R.id.open_explicit_intent);
+        mDoExplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textEntered = mEntryText.getText().toString();
+                Intent intent = new Intent(getApplicationContext(), ChildActivity.class);
+                intent.putExtra(Intent.EXTRA_TEXT, textEntered);
+                Toast.makeText(MainActivity.this, "You entered " + textEntered, Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
         mOpenGithubProject = (Button) findViewById(R.id.open_github_project);
         mOpenGithubProject.setOnClickListener(new View.OnClickListener() {
             @Override
